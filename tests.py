4576c64296b112229b7bd218a64936b1a18aec8e
kimymania <kimymania@gmail.com>
2025-07-19 22:29:05 +0900

    Added file content reader function

--- tests.py
+++ tests.py
@@ -1,26 +1,27 @@
 import unittest
 
-from functions.get_files_info import get_files_info
+# from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 class TestFileGetter(unittest.TestCase):
     def test1(self):
-        result = get_files_info("calculator", ".")
+        result = get_file_content("calculator", "main.py")
         self.assertTrue(result, result.startswith("Error: "))
         print(f"Result for current directory:\n{result}")
 
     def test2(self):
-        result = get_files_info("calculator", "pkg")
+        result = get_file_content("calculator", "pkg/calculator.py")
         self.assertTrue(result, result.startswith("Error: "))
         print(f"Result for 'pkg' directory:\n{result}")
 
     def test3(self):
-        result = get_files_info("calculator", "/bin")
+        result = get_file_content("calculator", "/bin/cat")
         self.assertTrue(result, result.startswith("Error: "))
         print(f"Result for '/bin' directory:\n{result}")
 
     def test4(self):
-        result = get_files_info("calculator", "../")
+        result = get_file_content("calculator", "pkg/does_not_exist.py")
         self.assertTrue(result, result.startswith("Error: "))
         print(f"Result for '../' directory:\n{result}")
 
